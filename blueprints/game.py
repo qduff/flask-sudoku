7f1b53c2dea3b1e4dea47582077ef4e8bf05f595
Quentin Duff <30555853+quizwammer@users.noreply.github.com>
2020-11-20 12:13:22 +0000

    Fix stuff

--- blueprints/game.py
+++ blueprints/game.py
@@ -1,16 +1,22 @@
-from flask import Blueprint, render_template
-from flask_login.utils import login_required
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login.utils import login_required, current_user
 
 game = Blueprint('game', __name__)
 
+
+#swap both
 @game.route('/hostgame')
 @login_required
 def hostgame():
-    return render_template('hostgame.html')
+    if not current_user.is_guest():
+        return render_template('hostgame.html')
+    else:
+        return redirect(url_for('main.index'))
 
 @game.route('/joingame')
 @login_required
 def joingame():
     return render_template('joingame.html')
+    
 
 #Add joingame POST and hostgame POST
