05a70b6f4fb93c3e4069e001e95ca8e7c40be5e4
Quentin Duff <30555853+quizwammer@users.noreply.github.com>
2020-11-20 18:51:12 +0000

    Start rooms

--- blueprints/game.py
+++ blueprints/game.py
@@ -22,8 +22,12 @@ def hostgame():
 def hostgame_post():
     roomname = request.form.get('roomname')
     codelen = 10
-    roomcode = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, codelen)])
-
+    roomcode = 0
+    while roomcode != 0 and roomcode not in games:
+        roomcode = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, codelen)])
+    print(roomcode)
+    
+    
 @game.route('/joingame')
 @login_required
 def joingame():
@@ -32,19 +36,39 @@ def joingame():
 @game.route('/joingame', methods=['POST'])
 def joingame_post():
     try:
-        return redirect(url_for('game.gamepage',id=int(request.form.get('gamecode'))))#+'/'+request.form.get('gamecode'))
-    except:
+        return redirect(url_for('game.lobby',id=int(request.form.get('gamecode'))))#+'/'+request.form.get('gamecode'))
+    except Exception as e:
         flash('Invalid input!')
+        print(e)
         return redirect(url_for('game.joingame'))
 
 
 @game.route('/game/<id>')
 @login_required
-def gamepage(id):
+def lobby(id:int):
     global games
     print(id,games)
     if int(id) in games:
-        return render_template('sudoku.html')
+        if games[int(id)]['started'] == True:
+            flash('That game has already started!')
+            return redirect(url_for('game.joingame'))
+        return render_template('lobby.html')
+    else:
+        flash('That game ID does not exist!')
+        return redirect(url_for('game.joingame'))
+
+
+@game.route('/play/<id>')
+@login_required
+def playpage(id):
+    global games
+    print(id,games)
+    if int(id) in games:
+        if games[int(id)]['started'] == True:
+            return render_template('sudoku.html')
+        else:
+            flash("That game hasn't started!")
+            return redirect(url_for('game.lobby',id=id))
     else:
         flash('That game ID does not exist!')
         return redirect(url_for('game.joingame'))
