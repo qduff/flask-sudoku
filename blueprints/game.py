bd2dd004f019fd4e0b651a52c785a6a1ef59dd22
Quentin Duff <30555853+quizwammer@users.noreply.github.com>
2020-11-22 12:53:08 +0000

    comments

--- blueprints/game.py
+++ blueprints/game.py
@@ -18,18 +18,21 @@ def hostgame():
     if not current_user.is_guest():
         return render_template('hostgame.html')
     else:
+        # non logged in users cannot host
         return redirect(url_for('main.index'))
     
 @game.route('/hostgame', methods=['POST'])
 def hostgame_post():
-    #print(current_user.id['username'])
     roomname = request.form.get('roomname')
     codelen = 4
     roomcode = 0
+    # Make roomcode
     while roomcode == 0 or roomcode in games:
         roomcode = ''.join(["{}".format(random.randint(1, 9)) for num in range(0, codelen)])
     print(f"New Room ({roomname}) created by {current_user.id['username']}\tCode:{roomcode}")
+    #Create a room in the games dict
     games.update({int(roomcode):{'name':roomname,'started':False,'players':{current_user.id['username']:{'completed':False,'admin':True}}}})
+    #redirect to lobby
     return redirect(url_for('game.lobby',id=roomcode))
     
     
@@ -41,10 +44,10 @@ def joingame():
 @game.route('/joingame', methods=['POST'])
 def joingame_post():
     try:
+        #Send to lobby with code
         return redirect(url_for('game.lobby',id=int(request.form.get('gamecode'))))#+'/'+request.form.get('gamecode'))
     except Exception as e:
         flash('Invalid input!')
-        print(e)
         return redirect(url_for('game.joingame'))
 
 
@@ -52,16 +55,18 @@ def joingame_post():
 @login_required
 def lobby(id:int):
     global games
-    #print(id,games)
     if int(id) in games:
+        #Check if game has started
         if games[int(id)]['started'] == True:
             flash('That game has already started!')
             return redirect(url_for('game.joingame'))
+        
+        #If not...
         else:
+            #If user has never joined this game, he will be added to dict.
             if current_user.dict['username'] not in games[int(id)]['players']:
                 games[int(id)]['players'].update({current_user.id['username']:{'completed':False,'admin':False}})
-            print(games)
-                
+            #If he is the admin, then he will get admin stuff!
             if games[int(id)]['players'][current_user.dict['username']]['admin'] == True:
                 admin = True
             else:
@@ -79,16 +84,15 @@ def lobby(id:int):
 def playpage(id):
     global games
     print(id,games)
+    # Check if game exists
     if int(id) in games:
+        # check if game has started
         if games[int(id)]['started'] == True:
             return render_template('sudoku.html')
         else:
+            # If not started, send to lobby with same id
             flash("That game hasn't started!")
             return redirect(url_for('game.lobby',id=id))
     else:
         flash('That game ID does not exist!')
         return redirect(url_for('game.joingame'))
-    
-
-#Add joingame POST and hostgame POST
-
