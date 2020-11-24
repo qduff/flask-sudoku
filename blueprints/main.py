64a99166150898ef6fc3abb96467b19aabdcbbdb
Quentin Duff <30555853+quizwammer@users.noreply.github.com>
2020-11-24 11:57:45 +0000

    fix

--- blueprints/main.py
+++ blueprints/main.py
@@ -6,6 +6,6 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@main.route('/')
+@main.route('/about')
 def about():
     return render_template('about.html')
