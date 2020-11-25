ba270fbe403144f5e1bcecfffd6883823a0e363b
Quentin Duff <30555853+quizwammer@users.noreply.github.com>
2020-11-25 13:05:52 +0000

    comet

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from socketmgr import socketio, app
 
 
 if __name__ == "__main__":
-    print('\n\n\nRunning\n-----------------')
+    print('\n\n\nRunning\n'+'-'*30)
     socketio.run(app, host='0.0.0.0', port=80, use_reloader=False, log_output=False, debug=True)
