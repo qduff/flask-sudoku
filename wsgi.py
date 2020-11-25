36b4eaefcb468d05f8bd1d631f83796aafbd5b20
Quentin Duff <30555853+quizwammer@users.noreply.github.com>
2020-11-25 13:03:54 +0000

    moar

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,5 @@
-from logging import debug
-from os import terminal_size
-from flask.globals import current_app
-
 from socketmgr import socketio, app
 
-import logging #Why no work ಥ_ಥ
-logging.getLogger('socketio').setLevel(logging.ERROR)
-logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 if __name__ == "__main__":
     print('\n\n\nRunning\n-----------------')
